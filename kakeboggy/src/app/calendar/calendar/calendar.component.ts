54f3346fe8110bbeb4fa05adb7b8b7d47774633b
Luis <luis.riverahuaman@hotmail.com>
2019-04-01 18:15:14 -0500

    pruebas de codigo calendario

--- kakeboggy/src/app/calendar/calendar/calendar.component.ts
+++ kakeboggy/src/app/calendar/calendar/calendar.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger, animation} from   '@angular/animations'
+import { formatDate } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+
+import localeEs from '@angular/common/locales/es'
+
+registerLocaleData(localeEs, 'es');
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.sass']
+  styleUrls: ['./calendar.component.sass'],
+  animations:[
+    trigger('detail expand', [
+      state('collapsed', style ({ height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class CalendarComponent implements OnInit {
+  //declaracion de arrays
+  nroDays = [30, 31, 28];
 
+  //fecha actual
+  today: number = Date.now();
+
+  
   constructor() { }
 
   ngOnInit() {
+    this.cuentaArray()
   }
   
-}
+  cuentaArray() {
+    let mes = formatDate(this.today,'MMMM','es','+0430');
+    var cantDays : number  
+    switch (mes) {
+      case 'enero': 
+      case 'marzo':
+        break;
+      case 'abril':
+        cantDays = this.nroDays[0];
+        break;
+    }
+    return console.log(cantDays);
+  }
+
+} 
